0d4728bb669e624ea64f6783c84fd99cce23bcec
rochamanuela <rochadesouzamanuela@gmail.com>
2024-05-10 11:29:20 -0300

    feat: adjustes realizeds

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -1,6 +1,6 @@
 import { ClimateService } from '../../Services/climate-service.service';
 import { Component, inject } from '@angular/core';
-import { 
+import {
   FormBuilder,
   FormsModule,
   ReactiveFormsModule,
@@ -16,25 +16,45 @@ import { ClimateInterface } from '../../Interfaces/climate-interface';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent{
+
+export class HomeComponent {
   cityName = "";
-  cityData:any = {};
+  climate: ClimateInterface = {} as ClimateInterface;
 
   _fb = inject(FormBuilder);
   _climateService = inject(ClimateService);
 
-  //constructor(private climateService: ClimateServiceService) {} // Injetando o serviço de clima
   onSubmit() {
     console.log(this.cityName);
-    if (this.cityName!="") {  
+    if (this.cityName != "") {
       this._climateService.getWeatherByCity(this.cityName || "campinas")
-        .subscribe((res:any)=>{
-            this.cityData = res;
-            // Aqui você pode manipular os dados do clima retornados pela API
-            console.log(res); 
-          }
+        .subscribe((res: any) => {
+          this.climate = {
+            lon: res.coord.lon,
+            lat: res.coord.lat,
+            description: res.weather[0].description,
+            temp: res.main.temp,
+            temp_min: res.main.temp_min,
+            temp_max: res.main.temp_max,
+            humidity: res.main.humidity,
+            name: res.name,
+            feels_like: res.main.feels_like
+          };
+          console.log(this.climate);
+        }
         );
+    } else {
+      this.climate = {
+        lon: 0,
+        lat: 0,
+        name: 'Campinas',
+        temp: 25,
+        description: 'Ensolarado',
+        temp_min: 20,
+        temp_max: 30,
+        feels_like: 26,
+        humidity: 50
+      };
     }
-    // agora só falta retornar esses dados obtidos para o usuário e ajustar as estilizações
   }
 }
